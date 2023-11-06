8646ff6631f2b222cc55657e53ffda9ff8537ce9
Hristina Adamovic <hristinacina@gmail.com>
2023-11-06 17:15:41 +0100

    added Poppins as font family

--- smarthome-front/src/index.js
+++ smarthome-front/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import googleFonts from 'google-fonts';
+
+googleFonts.add({
+  Poppins: ['300', '400', '500', '600', '700'],
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
