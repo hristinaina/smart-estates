b3b54435cd40f2688eaa0382a9b6be5b923dd595
savic-a <anastasijas557@gmail.com>
2023-11-13 14:59:35 +0100

    Added routes on front

---
+++ smarthome-front/src/AppRoutes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Login from "./components/Login/Login";
+
+const AppRoutes = [
+  {
+    path: '/',
+    element: <Login />
+  }
+];
+
+export default AppRoutes;
---
+++ smarthome-front/src/components/Login/Login.js
@@ -0,0 +1,13 @@
+// src/components/Login/Login.js
+import React from 'react';
+import './Login.css'; // Uvozite CSS datoteku
+
+const Login = () => {
+  return (
+    <div className='background'>
+      
+    </div>
+  );
+};
+
+export default Login;
