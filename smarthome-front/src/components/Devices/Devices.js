036c548c23bdcc469fb6a7b10acc64969527e6d4
Hristina Adamovic <hristinacina@gmail.com>
2023-11-15 00:19:54 +0100

    designed devices page

--- smarthome-front/src/components/Devices/Devices.js
+++ smarthome-front/src/components/Devices/Devices.js
@@ -3,23 +3,25 @@ import { Component } from 'react';
 import './Devices.css';
 import { Navigation } from '../Navigation/Navigation';
 import DeviceService from '../../services/DeviceService'
+import { Divider } from '@mui/material';
+import { width } from '@mui/system';
 
 export class Devices extends Component {
     constructor(props) {
         super(props);
         this.state = {
             data: [],
-          };
+        };
     }
 
     async componentDidMount() {
         try {
-          const result = await DeviceService.getDevices(1);
-          this.setState({ data: result });
+            const result = await DeviceService.getDevices(1);
+            this.setState({ data: result });
         } catch (error) {
-          // Handle error
+            // Handle error
         }
-      }
+    }
 
     render() {
         const { data } = this.state;
@@ -27,10 +29,15 @@ export class Devices extends Component {
         return (
             <div>
                 <Navigation />
-                <p id="add-real-estate">
-                    <img alt="." src="/images/plus.png" id="plus" />
-                    Add New Device
-                </p>
+                <div id="tools">
+                    <img src='/images/arrow.png' id='arrow' />
+                    <span className='estate-title'>Ta i ta nekretnina</span>
+                    <p id="add-device">
+                        <img alt="." src="/images/plus.png" id="plus" />
+                        Add Device
+                    </p>
+                </div>
+                <Divider style={{width: "87%", marginLeft: 'auto', marginRight: 'auto', marginBottom: '20px'}}/>
                 <DevicesList devices={data} />
             </div>
         )
@@ -39,35 +46,35 @@ export class Devices extends Component {
 
 const DevicesList = ({ devices }) => {
     const chunkSize = 5; // Number of items per row
-  
+
     const chunkArray = (arr, size) => {
-      return Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
-        arr.slice(i * size, i * size + size)
-      );
+        return Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
+            arr.slice(i * size, i * size + size)
+        );
     };
-  
+
     const rows = chunkArray(devices, chunkSize);
-  
+
     return (
-      <div id='devices-container'>
-        {rows.map((row, rowIndex) => (
-          <div key={rowIndex} className='device-row'>
-            {row.map((device, index) => (
-              <div key={index} className='real-estate-card'>
-                <img
-                  alt='device'
-                  src={device.image} 
-                  className='device-img'
-                />
-                <div className='device-info'>
-                  <p className='device-title'>{device.name}</p>
-                  <p className='device-text'>{device.type}</p>
-                  <p className='device-text state-color'>{device.status}</p>
+        <div id='devices-container'>
+            {rows.map((row, rowIndex) => (
+                <div key={rowIndex} className='device-row'>
+                    {row.map((device, index) => (
+                        <div key={index} className='device-card'>
+                            <img
+                                alt='device'
+                                src={device.image}
+                                className='device-img'
+                            />
+                            <div className='device-info'>
+                                <p className='device-title'>{device.name}</p>
+                                <p className='device-text'>{device.type}</p>
+                                <p className='device-text state-color'>{device.status}</p>
+                            </div>
+                        </div>
+                    ))}
                 </div>
-              </div>
             ))}
-          </div>
-        ))}
-      </div>
+        </div>
     );
-  };
+};
