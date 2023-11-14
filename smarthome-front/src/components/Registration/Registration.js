23abd14acc2c2018305dbffd9ea35079d3e3accf
savic-a <anastasijas557@gmail.com>
2023-11-14 18:18:30 +0100

    Added form validation for registration

--- smarthome-front/src/components/Registration/Registration.js
+++ smarthome-front/src/components/Registration/Registration.js
@@ -11,10 +11,19 @@ import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import './Registration.css';
 
 const Registration = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+
     const [showPassword, setShowPassword] = useState(false);
     const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
     const [profileImage, setProfileImage] = useState(null);
     const [imagePreview, setImagePreview] = useState(null);
+
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const passwordRegex = /^(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?])(.{8,})$/;
 
     const handleImageChange = (event) => {
       const file = event.target.files[0];
@@ -42,18 +51,54 @@ const Registration = () => {
         event.preventDefault();
     };
 
+    const handleEmailChange = (event) => {
+      setEmail(event.target.value);
+      event.target.value.trim() ===  '' || !emailRegex.test(event.target.value.trim()) || password.trim() === '' || confirmPassword.trim() === '' || confirmPassword !== password
+      ? checkButtonDisabled(true) : checkButtonDisabled(false)
+    };
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+        event.target.value.trim() ===  '' || !passwordRegex.test(event.target.value.trim()) || isValidEmail() || confirmPassword.trim() === '' || event.target.value.trim() !== confirmPassword
+        ? checkButtonDisabled(true) : checkButtonDisabled(false)
+    };
+
+    const handleConfirmPasswordChange = (event) => {
+      setConfirmPassword(event.target.value);
+      event.target.value.trim() ===  '' || !passwordRegex.test(event.target.value.trim()) || isValidEmail() || password.trim() === '' || event.target.value.trim() !== password
+      ? checkButtonDisabled(true) : checkButtonDisabled(false)
+    };
+
+    const isValidEmail = () => {
+      return email.trim() === '' || !emailRegex.test(email.trim())
+    }
+
+    const checkButtonDisabled = (value) => {
+        value ? setIsButtonDisabled(true) : setIsButtonDisabled(false);
+    };
+
+    const handleSignUp = () => {
+        // send values form to server
+    }
 
   return (
     <div className='ground'>
+
       <div className='left'>
         <p className='title-reg'>Sign up</p>
+
+        <form>
         <div className='input-fields'>
-            <div className='label'> Username:</div>
+            <div style={{marginRight: "250px"}}> Email:</div>
             <TextField
-                id="username"
+                value={email}
+                onChange={handleEmailChange}
+                id="email"
                 className='text-field'
                 sx={{ m: 1, width: '30ch' }}
-                placeholder="Type here"
+                placeholder="e.g. someone@example.com"
+                helperText="Required"
+                type='email'
             />
         </div>    
         <div className='input-fields'>
@@ -63,7 +108,10 @@ const Registration = () => {
             className='text-field'
             type={showPassword ? 'text' : 'password'}
             sx={{ m: 1, width: '30ch' }}
-            placeholder='Type here'
+            placeholder='e.g. !mikaMIKIC'
+            helperText="Required. Min 8 characters, special character, capital latter"
+            value={password}
+            onChange={handlePasswordChange}
             InputProps={{
               endAdornment: (
                 <InputAdornment position="end">
@@ -86,7 +134,10 @@ const Registration = () => {
             className='text-field'
             type={showConfirmPassword ? 'text' : 'password'}
             sx={{ m: 1, width: '30ch' }}
-            placeholder='Type here'
+            placeholder='e.g. !mikaMIKIC'
+            helperText="Required. Min 8 characters, special character, capital latter"
+            value={confirmPassword}
+            onChange={handleConfirmPasswordChange}
             InputProps={{
               endAdornment: (
                 <InputAdornment position="end">
@@ -119,8 +170,18 @@ const Registration = () => {
           </div>
         )}
 
-        <Button variant="contained" color="secondary" style={{marginTop: "50px", color: "#806894"}} sx={{ m: 1, width: '39ch' }}>Sign up</Button>
+        <Button 
+          variant="contained" 
+          color="secondary" 
+          disabled={isButtonDisabled}
+          onClick={handleSignUp}
+          style={{marginTop: "50px", color: "#806894"}} 
+          sx={{ m: 1, width: '39ch' }}>
+            Sign up
+        </Button>
+        </form>
       </div>
+
       <div className='right'>
         <p className='welcome'>Welcome to Smart Home!</p>
         <p className='desc'>One place to remotely manage all your devices!</p>
