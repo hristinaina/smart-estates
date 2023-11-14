c3f2a35ea071e3e2b4a6df9b48d7d371c858815a
kaca01 <96604086+kaca01@users.noreply.github.com>
2023-11-14 01:22:47 +0100

    Created card for real estate

--- smarthome-front/src/components/RealEstate/RealEstates.js
+++ smarthome-front/src/components/RealEstate/RealEstates.js
@@ -12,7 +12,20 @@ export class RealEstates extends Component {
         return (
             <div>
                 <p id="add-real-estate">
-                    <img alt="." src="/images/plus.png" id="plus" /> Add Real-Estate</p>
+                    <img alt="." src="/images/plus.png" id="plus" /> 
+                    Add Real-Estate
+                </p>
+                <div className='real-estate-card'>
+                    <img alt='real-estate' src='/images/real_estate_example.png' className='real-estate-img' />
+                    <div className='real-estate-info'>
+                        <p className='real-estate-title'>Villa B dorm</p>
+                        <p className='real-estate-text'>Location: Maldives</p>
+                        <p className='real-estate-text'>Square Footage: 102 m2</p>
+                        <p className='real-estate-text'>Number of Floors: 2</p>
+                        <p className='real-estate-text state-color'>Accepted</p>
+                    </div>
+                </div>
+                
             </div>
         )
     }
