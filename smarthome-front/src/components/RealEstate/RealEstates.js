3c1952a85cbaf0cc63c9213b2d9fe085a35b58fe
kaca01 <96604086+kaca01@users.noreply.github.com>
2023-11-14 01:35:06 +0100

    Putting cards next to each other

--- smarthome-front/src/components/RealEstate/RealEstates.js
+++ smarthome-front/src/components/RealEstate/RealEstates.js
@@ -15,14 +15,16 @@ export class RealEstates extends Component {
                     <img alt="." src="/images/plus.png" id="plus" /> 
                     Add Real-Estate
                 </p>
-                <div className='real-estate-card'>
-                    <img alt='real-estate' src='/images/real_estate_example.png' className='real-estate-img' />
-                    <div className='real-estate-info'>
-                        <p className='real-estate-title'>Villa B dorm</p>
-                        <p className='real-estate-text'>Location: Maldives</p>
-                        <p className='real-estate-text'>Square Footage: 102 m2</p>
-                        <p className='real-estate-text'>Number of Floors: 2</p>
-                        <p className='real-estate-text state-color'>Accepted</p>
+                <div id='real-estates-container'>
+                    <div className='real-estate-card'>
+                        <img alt='real-estate' src='/images/real_estate_example.png' className='real-estate-img' />
+                        <div className='real-estate-info'>
+                            <p className='real-estate-title'>Villa B dorm</p>
+                            <p className='real-estate-text'>Location: Maldives</p>
+                            <p className='real-estate-text'>Square Footage: 102 m2</p>
+                            <p className='real-estate-text'>Number of Floors: 2</p>
+                            <p className='real-estate-text state-color'>Accepted</p>
+                        </div>
                     </div>
                 </div>
                 
