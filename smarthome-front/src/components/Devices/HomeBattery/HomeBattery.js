67e66a907d4f50f0c1e21463740aa58feeba0084
Hristina Adamovic <hristinacina@gmail.com>
2023-12-18 00:41:34 +0100

    implemented battery graph on front

--- smarthome-front/src/components/Devices/HomeBattery/HomeBattery.js
+++ smarthome-front/src/components/Devices/HomeBattery/HomeBattery.js
@@ -1,4 +1,3 @@
-
 import { Component } from 'react';
 import '../Devices.css';
 import { Navigation } from '../../Navigation/Navigation';
@@ -6,9 +5,10 @@ import authService from '../../../services/AuthService';
 import DeviceService from '../../../services/DeviceService';
 import 'chart.js/auto';
 import SPGraph from '../SolarPanel/SPGraph';
-import { TextField } from '@mui/material';
-import { Button } from 'reactstrap';
-import { Snackbar } from "@mui/material";
+import HBGraph from './HBGraph';
+import AmbientSensorService from '../../../services/AmbientSensorService';
+import { Autocomplete, TextField, Button, Box, Grid, IconButton, Snackbar } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 
 
 export class HomeBattery extends Component {
@@ -19,12 +19,20 @@ export class HomeBattery extends Component {
         this.state = {
             device: {},
             data: [],
-            email: '',
-            startDate: '',
-            endDate: '',
+            activeGraph: 1,
             snackbarMessage: '',
             showSnackbar: false,
             open: false,
+            selectedOption: { label: '6h', value: '-6h' },
+            startDate: '',
+            endDate: '',
+            options: [
+                { label: '6h', value: '-6h' },
+                { label: '12h', value: '-12h' },
+                { label: '24h', value: '-24h' },
+                { label: 'last week', value: '-7d' },
+                { label: 'last month', value: '-30d' },
+            ],
         };
         this.id = parseInt(this.extractDeviceIdFromUrl());
         this.Name = "";
@@ -44,20 +52,35 @@ export class HomeBattery extends Component {
 
         const user = authService.getCurrentUser();
         this.Name = device.Device.Name;
-        const historyData = await DeviceService.getSPGraphData(this.id, user.Email, "2023-12-12", "2023-12-23");
 
+        const historyData = await AmbientSensorService.getGraphData(this.id);
+        const graphData = this.convertResultToGraphData(historyData.result);
         this.setState({
             device: updatedData,
-            data: historyData,
-            email: user.Email,
-            startDate: "2023-12-12",
-            endDate: "2023-12-23",
+            data: graphData,
         });
 
         // Set up interval to fetch device data every minute
         this.apiRequestInterval = setInterval(() => {
             this.fetchDeviceData();
         }, 60000); // 60000 milliseconds = 1 minute
+
+        let socket = new WebSocket("ws://localhost:8082/consumption")
+        console.log("Attempting Websocket Connection")
+
+        socket.onopen = () => {
+            console.log("Successfully Connected")
+            socket.send(this.id)
+        }
+
+        socket.onclose = (event) => {
+            console.log("Socket Closed Connection: ", event)
+        }
+
+        socket.onmessage = (msg) => {
+            if (this.state.activeGraph === 1)
+                this.populateGraph(msg.data)
+        }
     }
 
     async fetchDeviceData() {
@@ -75,20 +98,91 @@ export class HomeBattery extends Component {
         clearInterval(this.apiRequestInterval);
     }
 
-    handleFormSubmit = async (e) => {
-        e.preventDefault();
 
-        const { email, startDate, endDate } = this.state;
-        console.log(email, startDate, endDate);
-        const historyData = await DeviceService.getSPGraphData(this.id, email, startDate, endDate);
+    isTimestampInLastHour = (timestamp) => {
+        const currentTimestamp = new Date();
+        const timestampDate = new Date(timestamp);
+
+        const timeDifference = currentTimestamp - timestampDate;
+
+        return timeDifference <= 3600000;
+    };
+
+    populateGraph = (message) => {
+        const { data } = this.state;
+
+        const newValue = JSON.parse(message);
+        const timestamps = data.timestamps.filter((label) => this.isTimestampInLastHour(label)).concat(newValue.timestamp);
+        const consumptionData = [...data.consumptionData[0].data, newValue.consumption];
+        const updatedChartData = {
+            timestamps: timestamps,
+            consumptionData: consumptionData
+        }
         this.setState({
-            data: historyData,
+            data: updatedChartData,
         });
+    };
+
+    updateGraph = async (value) => {
+        const result = await AmbientSensorService.getDataForSelectedTime(this.id, value);
+        const graphData = this.convertResultToGraphData(result.result.result)
+        this.setState({
+            data: graphData,
+        });
+    }
+
+    setActiveGraph = (graphNumber) => {
+        this.setState({ activeGraph: graphNumber });
+    }
+
+    handleOptionChange = async (event, value) => {
+        this.setState({ selectedOption: value });
+        await this.updateGraph(value.value)
+    };
+
+    handleDateChange = (fieldName, event) => {
+        this.setState({ [fieldName]: event.target.value });
+    };
+
+    handleButtonClick = async () => {
+        if (this.state.startDate === '' || this.state.endDate === '') {
+            this.setState({ snackbarMessage: "Please enter dates" });
+            this.handleClick();
+            return;
+        }
+
+        if (new Date(this.state.startDate) > new Date(this.state.endDate)) {
+            this.setState({ snackbarMessage: "Start date cannot be greater than end date" });
+            this.handleClick();
+            return;
+        }
+        const oneMonth = 30 * 24 * 60 * 60 * 1000;
+        const difference = new Date(this.state.endDate) - new Date(this.state.startDate);
+
+        if (difference > oneMonth) {
+            this.setState({ snackbarMessage: 'The difference between start date and end date must not be more than one month' });
+            this.handleClick();
+            return;
+        }
+        const result = await AmbientSensorService.getDataForSelectedDate(this.id, this.state.startDate, this.state.endDate);
+        console.log("datum graf ", result.result.result)
+        const graphData = this.convertResultToGraphData(result.result.result)
+        result.result.result != null ? await HBGraph(graphData) : await HBGraph([])
     };
 
     extractDeviceIdFromUrl() {
         const parts = window.location.href.split('/');
         return parts[parts.length - 1];
+    }
+
+    convertResultToGraphData(values) {
+        const timestamps = Object.keys(values);
+        const consumptionData = timestamps.map((timestamp) => values[timestamp].humidity);
+        const graphData = {
+            timestamps: timestamps,
+            consumptionData: consumptionData
+        }
+        return graphData
     }
 
     handleBackArrow() {
@@ -108,8 +202,8 @@ export class HomeBattery extends Component {
     };
 
     render() {
-        const { device, switchOn, data, email, startDate, endDate } = this.state;
-        //TODO CRTEZ BATERIJE I PRIKAZATI KOLIKO JE TRENUTNO POPUNJENA
+        const { device, data, startDate, endDate, selectedOption, options } = this.state;
+
         return (
             <div>
                 <Navigation />
@@ -129,26 +223,77 @@ export class HomeBattery extends Component {
                         {this.renderBatteryIcon(device.CurrentValue, device.Size)}
                     </div>
                     <div id='sp-right-card'>
-                        <p className='sp-card-title'>Switch History</p>
-                        <form onSubmit={this.handleFormSubmit} className='sp-container'>
-                            <label>
-                                Email:
-                                <TextField style={{ backgroundColor: "white" }} type="text" value={email} onChange={(e) => this.setState({ email: e.target.value })} />
-                            </label>
-                            <br />
-                            <label>
-                                Start Date:
-                                <TextField style={{ backgroundColor: "white" }} type="date" value={startDate} onChange={(e) => this.setState({ startDate: e.target.value })} />
-                            </label>
-                            <br />
-                            <label>
-                                End Date:
-                                <TextField style={{ backgroundColor: "white" }} type="date" value={endDate} onChange={(e) => this.setState({ endDate: e.target.value })} />
-                            </label>
-                            <br />
-                            <Button type="submit" id='sp-data-button'>Fetch Data</Button>
-                        </form>
-                        <SPGraph data={data} />
+                        <p className='sp-card-title'>This estate consumption</p>
+                        <span className='buttons'>
+                            <span onClick={() => this.setActiveGraph(1)} className={this.state.activeGraph === 1 ? 'active-button' : 'non-active-button'}>Real Time</span>
+                            <span onClick={() => { this.setActiveGraph(2); this.updateGraph(this.state.selectedOption.value) }} className={this.state.activeGraph === 2 ? 'active-button' : 'non-active-button'}>History</span>
+                        </span>
+                        {this.state.activeGraph === 2 &&
+                            <div>
+                                <Grid container spacing={2}>
+                                    <Grid item xs={2}></Grid>
+                                    <Grid item xs={3}>
+                                        <Autocomplete
+                                            value={selectedOption}
+                                            onChange={this.handleOptionChange}
+                                            options={options}
+                                            getOptionLabel={(option) => option.label}
+                                            style={{ width: '100%' }}
+                                            renderInput={(params) => (
+                                                <TextField
+                                                    {...params}
+                                                    label="Select Time Range"
+                                                    InputLabelProps={{
+                                                        shrink: true,
+                                                    }}
+                                                />
+                                            )}
+                                            isOptionEqualToValue={(option, value) => option.value === value.value}
+                                            renderOption={(props, option, { selected }) => (
+                                                <li {...props}>
+                                                    <span>{option.label}</span>
+                                                </li>
+                                            )}
+                                            disableClearable />
+                                    </Grid>
+                                    <Grid item xs={6}>
+                                        <Box display="flex" alignItems="center" justifyContent="flex-end">
+                                            <TextField
+                                                label="Start Date"
+                                                type="date"
+                                                value={startDate}
+                                                onChange={(e) => this.handleDateChange('startDate', e)}
+                                                InputLabelProps={{
+                                                    shrink: true,
+                                                }}
+                                                inputProps={{
+                                                    max: new Date().toISOString().split('T')[0],
+                                                }}
+                                            />
+                                            <TextField
+                                                label="End Date"
+                                                type="date"
+                                                value={endDate}
+                                                onChange={(e) => this.handleDateChange('endDate', e)}
+                                                InputLabelProps={{
+                                                    shrink: true,
+                                                }}
+                                                inputProps={{
+                                                    max: new Date().toISOString().split('T')[0],
+                                                }}
+                                            />
+                                            <Button variant="contained" color="primary" onClick={this.handleButtonClick}>
+                                                Apply
+                                            </Button>
+                                        </Box>
+                                    </Grid>
+                                </Grid>
+
+                            </div>}
+                            <div className='canvas'>
+                    {this.state.activeGraph === 1 && <HBGraph data={data} />}
+                    {this.state.activeGraph === 2 && <HBGraph data={data}/>}
+                </div>
                     </div>
                 </div>
                 <Snackbar
@@ -192,12 +337,12 @@ export class HomeBattery extends Component {
         };
 
         return (
-        <div style={{marginTop: "50px", display: "flex", alignItems: "center", justifyContent: "center" }}>
-            <div style={{fontSize: "20px", display: "inline", marginRight: "10px"}}>{percentageOccupied}%</div>
-            <div style={batteryStyle}>
-                <div style={filledStyle}></div>
+            <div style={{ marginTop: "50px", display: "flex", alignItems: "center", justifyContent: "center" }}>
+                <div style={{ fontSize: "20px", display: "inline", marginRight: "10px" }}>{percentageOccupied}%</div>
+                <div style={batteryStyle}>
+                    <div style={filledStyle}></div>
+                </div>
             </div>
-        </div>
         );
     }
 }
