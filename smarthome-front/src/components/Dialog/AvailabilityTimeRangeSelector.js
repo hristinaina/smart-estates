0ea3087da106548bc8c267d83d940c6ecdeb9a26
kaca01 <96604086+kaca01@users.noreply.github.com>
2024-06-13 23:48:16 +0200

    Bind front and back

--- smarthome-front/src/components/Dialog/AvailabilityTimeRangeSelector.js
+++ smarthome-front/src/components/Dialog/AvailabilityTimeRangeSelector.js
@@ -5,6 +5,7 @@ import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { Line } from "react-chartjs-2";
 import DeviceAvailabilityService from '../../services/DeviceAvailabilityService';
+import { format } from 'date-fns';
 
 
 const AvailabilityTimeRangeSelector = ({onConfirm, onCancel}) => {
@@ -16,18 +17,30 @@ const AvailabilityTimeRangeSelector = ({onConfirm, onCancel}) => {
 
     const [isGraphVisible, setIsGraphVisible] = useState(false);
 
-    const testData = {
+    const [chartData, setChartData] = useState({
         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
         datasets: [
-          {
-            label: 'Monthly Sales',
-            data: [65, 59, 80, 81, 56, 55, 40],
-            fill: false,
-            borderColor: 'rgba(75,192,192,1)',
-            borderWidth: 2,
-          },
+            {
+                label: 'Device availability',
+                data: [65, 59, 80, 81, 56, 55, 40],
+                fill: false,
+                borderColor: 'rgba(75,192,192,1)',
+                borderWidth: 2,
+            },
         ],
-      };
+    });
+
+    const fetchNewData = async (labels, data) => {
+        // Replace this with your actual data fetching logic
+        setChartData(prevState => ({
+            ...prevState,
+            labels: labels,
+            datasets: prevState.datasets.map(dataset => ({
+                ...dataset,
+                data: data,
+            })),
+        }));
+    };
 
     const getDeviceId = () => {
         const parts = window.location.href.split('/');
@@ -38,7 +51,7 @@ const AvailabilityTimeRangeSelector = ({onConfirm, onCancel}) => {
     const handleTimeRangeChange = (field, value) => {
         setSelectedTimeRange(prevState => ({
         ...prevState,
-        [field]: value,
+        [field]: value ? value : null,
         }));
     };
 
@@ -57,10 +70,24 @@ const AvailabilityTimeRangeSelector = ({onConfirm, onCancel}) => {
     const confirm = async() => {
         // TODO: communication with backend
         console.log("Selected value:", selectedTimeRange.lastXitem);
+        console.log(selectedTimeRange.startTime);
+        console.log(selectedTimeRange.endTime);
 
-        let data = await DeviceAvailabilityService.get(getDeviceId(), "-" + selectedTimeRange.lastXitem, "-1");
-        console.log("DATAAAAAA");
-        console.log(data);
+        if ((selectedTimeRange.startTime == '') || (selectedTimeRange.endTime == '')) {
+            let data = await DeviceAvailabilityService.get(getDeviceId(), "-" + selectedTimeRange.lastXitem, "-1");
+            console.log("DATAAAAAA");
+            console.log(data);
+            console.log(Object.keys(data));
+            fetchNewData(Object.keys(data), Object.values(data));
+        } else {
+            let data = await DeviceAvailabilityService.get(getDeviceId(), selectedTimeRange.startTime.toISOString(), 
+                                                           selectedTimeRange.endTime.toISOString());
+            console.log("DATAAAAAA");
+            console.log(data);
+            handleTimeRangeChange("startTime", "");
+            handleTimeRangeChange("endTime", "");
+        }
+
 
         setIsGraphVisible(true);
     };
@@ -81,22 +108,24 @@ const AvailabilityTimeRangeSelector = ({onConfirm, onCancel}) => {
                             <DatePicker
                                 className='picker'
                                 label="Starting point"
-                                // value={startDate}
-                                // onChange={handleStartDateChange}
+                                value={selectedTimeRange.startDate || null}
+                                onChange={(date) => handleTimeRangeChange('startTime', date)}
+                                format="MM/dd/yyyy"
                             />
                             <p></p>
                             <DatePicker
                                 className='picker'
                                 label="Ending point"
-                                // value={endDate}
-                                // onChange={handleEndDateChange}
+                                value={selectedTimeRange.endDate || null}
+                                onChange={(date) => handleTimeRangeChange('endTime', date)}
+                                format="MM/dd/yyyy"
                             />
                             <p></p>
                             {/* {displayError && <p id='please-choose-dates'>{errorMessage}</p>} */}
                             
                             <div>
                                 <p><b>OR</b> choose predefined time range:</p>
-                                {['6h', '12h', '24h', '1w', '1m'].map(item => (
+                                {['6h', '12h', '24h', '1w', '30d'].map(item => (
                                     <label key={item} style={{marginRight: '10px'}}>
                                         <input
                                             type="radio"
@@ -117,11 +146,15 @@ const AvailabilityTimeRangeSelector = ({onConfirm, onCancel}) => {
                    
                     {/* <button onClick={() => onConfirm(selectedDate)}>CONFIRM</button> */}
                     {isGraphVisible && (
-                        <div id="dialog">
+                        <div id="dialog" >
                             <p id="dialog-title">Availability graph</p>
-                            <p id='dialog-message'>Start Date: 2023-12-31</p>
-                            <p id='dialog-message'>End Date: 2024-01-07</p>
-                            <Line data={testData}/>
+                            <div>
+                                <p id='dialog-message'>Start Date: {format(selectedTimeRange.startTime, 'MMMM do, yyyy')}</p>
+                                <p id='dialog-message'>End Date: {format(selectedTimeRange.endTime, 'MMMM do, yyyy')}</p>
+                            </div>
+                            <div style={{ width: '800px', height: '400px' }}>
+                            <Line data={chartData}/>
+                            </div>
                             <br/>
                             <button onClick={close}>CLOSE</button>
                             <button onClick={goBack}>GO BACK</button>
